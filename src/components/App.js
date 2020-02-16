8f73585033a14dac6111c7d63792c929f4da3869
Yair Facio <yair.facio11@gmail.com>
2020-02-16 12:11:31 -0600

    Fetching data from the app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { fetchActivities, fetchActivityLogs, fetchBabies, fetchAssistants } from '../actions'
 
 export class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchActivities();
+    this.props.fetchActivityLogs();
+    this.props.fetchAssistants();
+    this.props.fetchBabies();
+  }
 
   render() {
     return (
@@ -12,5 +20,11 @@ export class App extends Component {
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps, { fetchActivities, fetchActivityLogs, fetchAssistants, fetchBabies})(App)
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux'
+import activitiesReducer from './activitiesReducer'
+import babiesReducer from './babiesReducer'
+import assistantsReducer from './assistantsReducer'
+import activityLogsReducer from './activityLogsReducer'
 
 export default combineReducers({
-  fdfsd: () => 33
+  activities: activitiesReducer,
+  babies: babiesReducer,
+  assistants: assistantsReducer,
+  activityLogs: activityLogsReducer
 })
