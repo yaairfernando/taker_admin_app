8de17e1bea61bfaa88f6e67e2f2a4e48add8f6c6
Yair Facio <yair.facio11@gmail.com>
2020-02-16 11:39:13 -0600

    Setting up index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App';
 
+import { Provider } from 'react-redux'
+import reducers from './reducers/index'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
-  <App />,
+  <Provider store={store} >
+    <App />
+  </Provider>,
   document.querySelector("#root")
 );
