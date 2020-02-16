f79995a07e3d63bd3afd0e616a8587379af836e4
Yair Facio <yair.facio11@gmail.com>
2020-02-16 11:35:01 -0600

    Setting up main components and installing dependencies

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
+}
+
+export default App
+
